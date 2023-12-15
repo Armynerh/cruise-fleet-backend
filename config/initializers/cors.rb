b0b533446a6eec16d598cdf79a20bba8215024a1
ahmed025566 <ahmedsalah025566#gmail.com>
2023-12-15 14:06:02 +0200

    Add my authentication system

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*",
+    origins "http://localhost:3000",
+            "http://localhost:5173"
             "our production front end domain"
     resource "*",
     headers: :any,
