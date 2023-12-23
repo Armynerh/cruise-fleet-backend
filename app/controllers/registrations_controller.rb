f9f5ff0e32706072ae02e513e9c33bb37509e24e
Armynerh <aimenah@outlook.com>
2023-12-23 14:58:14 -0500

    fix errors in controllers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,18 @@
 class RegistrationsController < ApplicationController
-    def create
-      user_params = params.require(:user).permit(:email, :password, :password_confirmation)
-      user = User.create!(user_params)
-      # user = User.create!(
-      #   email: params['user']['email'],
-      #   password: params['user']['password'],
-      #   password_confirmation: params['user']['password_confirmation']
-      # )
-  
-      if user
-        session[:user_id] = user.id
-        render json: { status: 'created', user: }
-      else
-        render json: { status: 500 }
-      end
+  def create
+    user_params = params.require(:user).permit(:email, :password, :password_confirmation)
+    user = User.create!(user_params)
+    # user = User.create!(
+    #   email: params['user']['email'],
+    #   password: params['user']['password'],
+    #   password_confirmation: params['user']['password_confirmation']
+    # )
+
+    if user
+      session[:user_id] = user.id
+      render json: { status: 'created', user: }
+    else
+      render json: { status: 500 }
     end
   end
+end
