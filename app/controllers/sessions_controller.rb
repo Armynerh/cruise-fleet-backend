88ab1a88b6e08b05ec918f22d69314b6d4f8fd77
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-01-08 16:21:48 +0100

    Add before_action in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :set_current_user
+
   def create
     user = User.find_by(email: params['user']['email'])
       .try(:authenticate, params['user']['password'])
