a3a115b65672f9ed175c00d84d1bc7ef23cfe4d6
Armynerh <aimenah@outlook.com>
2024-01-08 15:42:41 -0500

    modify credentials

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,22 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-# Exit on error
-set -e
-
-# Install Ruby dependencies
-echo "Installing Ruby dependencies..."
 bundle install
-
-# Run database migrations
-echo "Running database migrations..."
-bundle exec rails db:migrate
-
-# Precompile assets
-echo "Precompiling assets..."
-bundle exec rails assets:precompile
-
-# Optionally, seed the database
-echo "Seeding the database..."
-bundle exec rails db:seed
-
-echo "Build script completed successfully."
+./bin/rails assets:precompile --trace
+./bin/rails assets:clean
