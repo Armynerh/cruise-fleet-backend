2e3cf38d27637b9e08a3f09e64b245be22cc8b49
Armynerh <aimenah@outlook.com>
2024-01-08 18:14:04 -0500

    change cors.rb again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
-            "https://cruise-fleet-baknd.onrender.com"
+            "https://cruise-fleet-baknd.onrender.com",
             "https://cruisefleet.onrender.com"
     resource "*",
-    headers: :any,
-    methods: [:get, :post, :put, :patch, :delete, :options, :head],
-     credentials: true,
-    max_age: 86400
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true,
+      max_age: 86400
   end
 end
